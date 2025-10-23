afdf97e6eff6bfb46ae068e3a38dd770f05492df
Mohd Adil <shaikhmdadil191@gmail.com>
2025-10-23 11:34:39 +0530

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/hero'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
